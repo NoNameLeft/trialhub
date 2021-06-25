dcfc5f636e1fb22b07b3508386e665e5debea120
NoNameLeft <stankmenta422@gmail.com>
2021-06-25 11:06:03 +0300

    Initial create

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+const Header = () => {
+    return (
+        <div className="navbar">
+            <div className="navbar__logo">
+                <h1>Logo</h1>
+            </div>
+            <div className="navbar__btns">
+                <h1>Buttons</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+const Home = () => {
+    return (
+        <div className="home">
+            <div className="home__text">
+                <h1>Text</h1>
+            </div>
+            <div className="home__img">
+                <h1>Image</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
